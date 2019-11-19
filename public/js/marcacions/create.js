c8b4ef3e4c2b97ee5ca2b6d36251c628bc9a2308
GLORIBEL KATHERINE DELGADO VALENZUELA <delgadogloribelv@gmail.com>
2019-11-19 07:52:50 -0500

    position

--- public/js/marcacions/create.js
+++ public/js/marcacions/create.js
@@ -8,6 +8,15 @@ var regexaño     = /^([0-9]{4})$/;
 
 $(document).ready(function() {
 
+    if ("geolocation" in navigator){ //check geolocation available
+    //try to get user current location using getCurrentPosition() method
+    navigator.geolocation.getCurrentPosition(function(position){
+      console.log("Found your location nLat : "+position.coords.latitude+" nLang :"+ position.coords.longitude);
+    });
+    }else{
+    console.log("Browser doesn't support geolocation!");
+    }
+
   if (navigator.geolocation) { //check if geolocation is available
       navigator.geolocation.getCurrentPosition(function(position){
         console.log(position.coords.latitude);
